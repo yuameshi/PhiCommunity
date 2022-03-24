7da74025e6788b2214b1daee916bd32d3c954dd5
HanHan <64469437+HanHan233@users.noreply.github.com>
2022-03-24 21:58:33 +0800

    在tapToStart处添加SW安装（解决APP无法缓存的问题（应该是没问题的（嗯

--- src/tapToStart/index.js
+++ src/tapToStart/index.js
@@ -46,3 +46,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 510);
 	});
 });
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('../service-worker.js')
+			.then((registration) => {
+				console.log('SW registered: ', registration);
+			})
+			.catch((registrationError) => {
+				console.log('SW registration failed: ', registrationError);
+			});
+	});
+}
