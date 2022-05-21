a1c871ca50d5c3d541eceea1fcc25735d72cc45a
HanHan233 <64469437+Yuameshi@users.noreply.github.com>
2022-05-21 14:55:50 +0800

    修复设置界面部分设置状态显示错误的问题

--- src/settings/index.js
+++ src/settings/index.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	if (window.localStorage.length == 0) {
 		document.querySelector('#backBtn').addEventListener('click', () => {
 			// location.href = '../chapterSelect/index.html';
-			location.href='../whilePlaying/index.html?play=tutorial&l=ez';
+			location.href = '../whilePlaying/index.html?play=tutorial&l=ez';
 		});
 	} else {
 		document.querySelector('#backBtn').addEventListener('click', () => {
@@ -36,24 +36,26 @@ window.addEventListener('DOMContentLoaded', () => {
 	settings.forEach((setting) => {
 		let item;
 		switch (setting.type) {
-		case 'slide':
-			setting.defaultValue =
-					parseFloat(window.localStorage.getItem(setting.codename)) ||
+			case 'slide':
+				setting.defaultValue =
+					Number(window.localStorage.getItem(setting.codename)) ||
 					setting.defaultValue;
-			item = SliderItem(setting);
-			break;
-		case 'toggle':
-			setting.defaultValue =
-					window.localStorage.getItem(setting.codename) == 'true'
-						? true
-						: false || setting.defaultValue;
-			item = ToggleItem(setting);
-			break;
-		case 'button':
-			item = ButtonItem(setting);
-			break;
-		default:
-			throw new Error('Unknown setting: ' + setting);
+				item = SliderItem(setting);
+				break;
+			case 'toggle':
+				if (window.localStorage.getItem(setting.codename) !== null) {
+					setting.defaultValue =
+						window.localStorage.getItem(setting.codename) == 'true'
+							? true
+							: false;
+				}
+				item = ToggleItem(setting);
+				break;
+			case 'button':
+				item = ButtonItem(setting);
+				break;
+			default:
+				throw new Error('Unknown setting: ' + setting);
 		}
 		document.getElementById('settingItems').appendChild(item.element);
 	});
